1006ac8eda06a61e0977cb925d27902b5d68c685
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2026-06-09 20:25:46 -0300

    vb

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+    expiresIn: process.env.JWT_EXPIRES_IN || '15d',
   });
 };
 
